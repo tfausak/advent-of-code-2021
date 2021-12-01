7ca647d4000b25bdaaaab46c8a5526be6af7a428
Taylor Fausak <taylor@fausak.me>
2021-12-01 14:28:45 +0000

    Solve day 1 part 2

--- 01-2.fsx
+++ 01-2.fsx
@@ -1,6 +1,8 @@
 // https://adventofcode.com/2021/day/1
 System.IO.File.ReadLines "input/1.txt"
     |> Seq.map int
+    |> Seq.windowed 3
+    |> Seq.map Seq.sum
     |> Seq.pairwise
     |> Seq.where (fun (x, y) -> y > x)
     |> Seq.length
